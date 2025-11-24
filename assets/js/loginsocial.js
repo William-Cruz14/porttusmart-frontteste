693d260678f763d891601b0182da116fc7d3b412
William Cruz <cruzwilliam14@gmail.com>
2025-11-24 03:58:40 -0300

    add point

--- assets/js/loginsocial.js
+++ assets/js/loginsocial.js
@@ -73,7 +73,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({
-                    code: code
+                    code: code,
                 })
             });
 
